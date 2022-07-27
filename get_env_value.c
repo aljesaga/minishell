19c031b80e0eec8ac7a8b348529e566a37be6de3
Iván Oriola <55549182+ivanoriola@users.noreply.github.com>
2022-07-27 10:39:47 +0200

    get_env_values
    Busca en la lista de "t_env" y devuelve el valor de la variable que le pidas.
    Si no la encuentra devuelve NULL.
    
    Ejemplo:
    {
    	char *pwd;
    	pwd = get_env_value("PWD", mini);
    }

---
+++ get_env_value.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_env_value.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioriola <ioriola@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 10:23:14 by ioriola           #+#    #+#             */
+/*   Updated: 2022/07/27 10:25:08 by ioriola          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+Busca en la lista de "t_env" y devuelve el valor de la variable que le pidas.
+Si no la encuentra devuelve NULL.
+
+Ejemplo:
+{
+	char *pwd;
+	pwd = get_env_value("PWD", mini);
+}
+*/
+
+char	*get_env_value(char *env_name,  t_mshell *mini)
+{
+	t_env	*temp_env;
+	char	*value;
+	int len;
+
+	len = ft_strlen(env_name);
+	temp_env = mini->env;
+	while (temp_env->value)
+	{
+		value = ft_strnstr(temp_env->value, env_name, len);
+		if (value)
+		{
+			value += 1 + len;
+			printf("%s: %s\n",env_name, value);
+			return (value);
+		}
+		if (!temp_env->next)
+			break;
+		else
+			temp_env = temp_env->next;
+	}
+	return (NULL);
+}
