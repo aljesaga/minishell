c7c539836f019e7ecb6d4e656d69d8e8bccb87ba
Iván Oriola <55549182+ivanoriola@users.noreply.github.com>
2022-07-29 09:29:53 +0200

    primer intento, falta mucho pero funciona

--- base_fts/ft_cd.c
+++ base_fts/ft_cd.c
@@ -3,32 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alsanche <alsanche@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: ioriola <ioriola@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 12:04:13 by alsanche          #+#    #+#             */
-/*   Updated: 2022/07/19 12:43:10 by alsanche         ###   ########lyon.fr   */
+/*   Updated: 2022/07/29 09:22:37 by ioriola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	ft_cd(char *str)
+int		ft_cd(char *line, t_mshell *mini)
 {
-	if (str[0] == '\0')
-		write(1, "/home/mi_usuario\n", 18);
-	else if (str[0] == '.' && str[1] == '.')
+	char *temp_pwd;
+	int chdir_;
+
+	temp_pwd = get_env_value("PWD", mini);
+	line += 3;
+	line = ft_strtrim(line, " ");
+	chdir_ = chdir(line);
+	if (chdir_ == -1)
 	{
-		if (str[2] == '/')
-			write(1, "/anterior/carpeta_en_ruta_anterior\n", 36);
-		else
-			write(1, "/ahora_aqui/actual\n", 20);
+		printf("Failed to change directory: %s\n", strerror(errno));
+		return (1);
 	}
-	else if (str)
-	{
-		write(1, "/actual/", 9);
-		printf("%s\n", str);
-	}
-	else
-		write(1, "ERROR\n", 6);
+	export(ft_strjoin("OLDPWD=", temp_pwd), mini);
+	export(ft_strjoin("PWD=", getcwd(NULL, 0)), mini);
+
+	// if (line[0] == '\0')
+	// 	write(1, "/home/mi_usuario\n", 18);
+	// else if (line[0] == '.' && line[1] == '.')
+	// {
+	// 	if (line[2] == '/')
+	// 		write(1, "/anterior/carpeta_en_ruta_anterior\n", 36);
+	// 	else
+	// 		write(1, "/ahora_aqui/actual\n", 20);
+	// }
+	// else if (line)
+	// {
+	// 	write(1, "/actual/", 9);
+	// 	printf("%s\n", line);
+	// }
+	// else
+	// 	write(1, "ERROR\n", 6);
 	return (0);
 }
