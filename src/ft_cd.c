fddc3f9c472d9a99a3b933b34a36fcbbedebc218
Iván Oriola <55549182+ivanoriola@users.noreply.github.com>
2022-07-31 11:57:27 +0200

    Corrección en lectura de "line"

--- src/ft_cd.c
+++ src/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ioriola <ioriola@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 12:04:13 by alsanche          #+#    #+#             */
-/*   Updated: 2022/07/30 16:39:06 by ioriola          ###   ########.fr       */
+/*   Updated: 2022/07/31 11:47:17 by ioriola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 	Change the shell working directory.
 		- With only a relative or absolute path
 */
+
+void	print_error(char *line)
+{
+	ft_putstr_fd("cd: ", 2);
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(line, 2);
+	ft_putchar_fd('\n', 2);
+}
 
 int		ft_cd(char *line, t_mshell *mini)
 {
@@ -29,7 +38,7 @@ int		ft_cd(char *line, t_mshell *mini)
 	chdir_ = chdir(line);
 	if (chdir_ == -1)
 	{
-		printf("cd: %s: %s\n", strerror(errno), line);
+		print_error(line);
 		return (1);
 	}
 	ft_export(ft_strjoin("OLDPWD=", temp_pwd), mini);
