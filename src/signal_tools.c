f577b6429ac77537a647e6df3f10c58a34651adb
ivanoriola <ivanoriola@gmail.com>
2022-12-21 10:59:07 +0100

    New Signal Handler

--- src/signal_tools.c
+++ src/signal_tools.c
@@ -6,7 +6,7 @@
 /*   By: ioriola <ioriola@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 09:04:01 by ioriola           #+#    #+#             */
-/*   Updated: 2022/12/21 10:55:09 by ioriola          ###   ########.fr       */
+/*   Updated: 2022/12/21 10:58:44 by ioriola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,17 @@ void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		signal(SIGINT, SIG_IGN);
 		printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 		//g_l_exit = 1
-		signal(SIGINT, &signal_handler);
 	}
 	else if (sig == SIGQUIT)
 	{
-		signal(SIGQUIT, SIG_IGN);
 		rl_on_new_line();
 		rl_replace_line(rl_line_buffer, rl_end);
 		rl_redisplay();
-		signal(SIGQUIT, &signal_handler);
 	}
 }
 
